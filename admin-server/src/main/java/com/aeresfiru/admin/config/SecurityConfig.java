f828b413db3fd831910b543ec6d2a54b697d35e2
Aliaksandr Shtarou <aeresfiru@gmail.com>
2024-05-23 14:34:38 +0300

    Add Docker configuration

--- admin-server/src/main/java/com/aeresfiru/admin/config/SecurityConfig.java
+++ admin-server/src/main/java/com/aeresfiru/admin/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.aeresfiru.admin.config;
 
 import com.aeresfiru.admin.web.client.OauthHttpHeadersProvider;
 import jakarta.annotation.Priority;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -9,12 +10,20 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
 import org.springframework.security.oauth2.client.AuthorizedClientServiceOAuth2AuthorizedClientManager;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.core.oidc.user.OidcUserAuthority;
 import org.springframework.security.web.SecurityFilterChain;
 
+import java.util.Collection;
+import java.util.HashSet;
+
 @Configuration
+@Slf4j
 public class SecurityConfig {
 
     @Bean
@@ -54,5 +63,29 @@ public class SecurityConfig {
                 .authorizeHttpRequests(customizer -> customizer
                         .anyRequest().hasRole("MANAGER"))
                 .build();
+    }
+
+    @Bean
+    public GrantedAuthoritiesMapper userAuthoritiesMapperForKeycloak() {
+        return authorities -> {
+            var mappedAuthorities = new HashSet<GrantedAuthority>();
+            var authority = authorities.iterator().next();
+
+            if (authority instanceof OidcUserAuthority oidcUserAuthority) {
+                var userInfo = oidcUserAuthority.getUserInfo();
+                if (userInfo.hasClaim("groups")) {
+                    var roles = userInfo.getClaimAsStringList("groups");
+                    mappedAuthorities.addAll(generateAuthoritiesFromClaim(roles));
+                }
+            }
+            return mappedAuthorities;
+        };
+    }
+
+    private Collection<? extends GrantedAuthority> generateAuthoritiesFromClaim(Collection<String> roles) {
+        return roles.stream()
+                .filter(role -> role.startsWith("ROLE_"))
+                .map(SimpleGrantedAuthority::new)
+                .toList();
     }
 }
